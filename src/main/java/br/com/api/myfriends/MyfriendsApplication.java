7737df8bfa879102a8a2816b9016ca4600a337f2
brunbs <babosa227@gmail.com>
2022-06-30 12:02:41 -0300

    First Commit

---
+++ src/main/java/br/com/api/myfriends/MyfriendsApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.myfriends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfriendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfriendsApplication.class, args);
+	}
+
+}
